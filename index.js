3501b53b979b3bde1878ccccddde98c1033a73fa
rompo <romain.schutz@he-arc.ch>
2018-08-30 09:31:27 +0200

    Adding incomming messages event

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-
 //The config file is not part of the repository because
 //your robot token must be kept secret.
 var config = require('./config/config-local.json');
 
+//Discord API
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 console.log('Hello, Discord Du Cul !');
 
 bot.login(config.robot.token);
+
+//Incomming messsage event
+bot.on('message', message =>
+{
+    if(message.content === "_ping")
+    {
+        message.reply("Du cul !")
+    }
+})
