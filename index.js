26edba882213ab8155ad27cdd6177c9c499b5cdd
rompo <romain.schutz@he-arc.ch>
2018-09-19 11:07:53 +0200

    Add 3 commands with random answers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //The config file is not part of the repository because
 //your robot token must be kept secret.
 var config = require('./config/config-local.json');
-var COMMAND_START = '_';
+var fs = require('fs');
+
+const COMMAND_PREFIX = '_';
+//const WORDS_COUNT = 22740;
 
 //Discord API
 const Discord = require('discord.js');
@@ -18,21 +21,39 @@ console.log('Logged in !');
 bot.on('message', message =>
 {
     //Avoid botception
-    if(message.author.bot)
+    if (message.author.bot)
         return;
-    
-    console.log(message.author+' send \''+message.content+'\'');
-    
-    //Check command
-    if(message.content.startsWith(COMMAND_START))
+
+    //Ignore non-command messages
+    if (!message.content.startsWith(COMMAND_PREFIX))
+        return;
+
+    //Log
+    console.log(message.author + ' sent \'' + message.content + '\'');
+
+    //Separate command and args
+    const command = message.content.substr(1);
+    const arguments = message.content.slice(COMMAND_PREFIX.length);
+
+    switch (command)
     {
-     var command = message.content.substr(1);
-     
-                switch (command)
-                {
-                    case "ping" :
-                        message.reply("Pong du cul !");
-                        break;
-                }
+        case "ping" :
+            message.reply("Pong du cul !");
+            break;
+        case "ducul" :
+            message.reply(getRandomLine().trim() + " du cul");
+            break;
+        case "demerde" :
+            message.reply(getRandomLine().trim() + " de merde");
+            break;
+        case "detesmorts" :
+            message.reply(getRandomLine().trim() + " de tes morts");
+            break;
     }
-})
+});
+
+//Get a random line from de dictionnary
+function getRandomLine() {
+    var lines = fs.readFileSync('./config/words_fr.txt').toString().split("\n");
+    return lines[Math.floor(Math.random() * lines.length)];
+};
