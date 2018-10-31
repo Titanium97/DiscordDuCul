ff4070c41c962fb1dc67b26be60febfd23f2b1de
rompo <romain.schutz@he-arc.ch>
2018-10-31 14:45:43 +0100

    ADD XP System

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 
 const config_local = require('./config/config-local.json');
 const config = require('./config/config.json');
+const config_save = require('./config/save.json');
 
 /*
  * Load modules
@@ -26,12 +27,76 @@ const discord = require('discord.js');
 
 const bot = new discord.Client();
 
-//Login
+/*
+ * Functions
+ */
+
+/***
+ * Get a random line from dictionnary
+ */
+function getRandomLine()
+{
+    var lines = fs.readFileSync('./config/'+config.dictionnary).toString().split("\n");
+    return lines[Math.floor(Math.random() * lines.length)].trim();
+};
+
+/***
+ * Get a random answer from config file
+ */
+function crystallBall()
+{
+    return config.answers[Math.floor(Math.random() * config.answers.length)];
+}
+
+/***
+ * Add XP to specific user
+ */
+function addXPUser(user, amount)
+{
+  if (user in config_save.xps)
+    config_save.xps[user] += amount;
+  else
+    config_save.xps[user] = amount;
+}
+
+/***
+ * Get specific user's XP
+ */
+function getXPUser(user)
+{
+  if (user in config_save.xps)
+    return config_save.xps[user];
+
+  return 0;
+
+}
+
+/***
+ * Save variables into JSON save file
+ */
+function save()
+{
+  data = JSON.stringify(config_save);
+
+  fs.writeFileSync("./config/save.json", data, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("Bot state is saved !");
+  });
+}
+
+/*
+ * Login
+ */
 console.log('Logging in...');
 bot.login(config_local.robot.token);
 console.log('Logged in !');
 
-//Incomming message event
+/*
+ * Incomming message event
+ */
 bot.on('message', message =>
 {
     //Avoid botception
@@ -61,21 +126,29 @@ bot.on('message', message =>
         case "ducul" :
         case "cul" :
             message.channel.send(getRandomLine() + " du cul");
+            addXPUser(message.author, config.xp.cul);
             break;
         case "demerde" :
         case "merde" :
             message.channel.send(getRandomLine() + " de merde");
+            addXPUser(message.author, config.xp.merde);
             break;
         case "detesmorts" :
         case "morts" :
             message.channel.send(getRandomLine() + " de tes morts");
+            addXPUser(message.author, config.xp.morts);
             break;
         case "question" :
         case "q" :
             message.reply(message.content.substr(command.length+1)+", "+crystallBall()+"...");
+            addXPUser(message.author, config.xp.question);
+            break;
+        case "xp" :
+            message.reply("You have "+getXPUser(message.author)+" XP");
             break;
         case "fuckingragequit" :
             message.channel.send("Oh no...");
+            //save();
             process.exit();
             break;
         default :
@@ -84,18 +157,8 @@ bot.on('message', message =>
 });
 
 /*
- * Functions
+ * Exit event
  */
-
-/***
- * Get a random line from dictionnary
- * @returns Random String from dictionnary
- */
-function getRandomLine() {
-    var lines = fs.readFileSync('./config/'+config.dictionnary).toString().split("\n");
-    return lines[Math.floor(Math.random() * lines.length)].trim();
-};
-
-function crystallBall() {
-    return config.answers[Math.floor(Math.random() * config.answers.length)];
-}
+process.on('exit', function () {
+   save();
+ });
